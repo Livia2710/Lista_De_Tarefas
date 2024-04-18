5fb9615e8221dd95a5e35c3cf23151b1bab8f5db
@Livia2710 <livia.sesi166@gmail.com>
2024-04-18 10:53:19 -0300

    finalização das funcionalidades

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   const [filter, setFilter] = useState("All");
-  const [sort, setSort] = useState("Asc");
+  const [sort, setSort] = useState("Cres");
 
   const removeTodo = (id) => {
     const newTodos = [...todos];
@@ -77,11 +77,11 @@ function App() {
          todo.text.toLowerCase().includes(search.toLowerCase())
       )
       .sort((a, b) =>
-       sort === "Asc"
+       sort === "Cres"
          ? a.text.localeCompare(b.text)
          : b.text.localeCompare(a.text)
         )
-        
+
         .map((todo)=>(
           <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
